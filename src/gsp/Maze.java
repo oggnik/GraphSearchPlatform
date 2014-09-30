d62457d892520f314eb1dd7f2a4b33d0cd21c3ca
Nicki Ogg <oggnicki@yahoo.com>
2014-09-30 14:42:01 -0400

    Nicer path printing

--- src/gsp/Maze.java
+++ src/gsp/Maze.java
@@ -84,7 +84,7 @@ public class Maze {
 		}
 		String[][] strMaze = getStringMaze();
 		for (Tile tile : path) {
-			strMaze[tile.getY()][tile.getX()] = "HI";
+			strMaze[tile.getY()][tile.getX()] = "██";
 		}
 		printMaze(strMaze);
 	}
